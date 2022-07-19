b269cda9e4f857dbc0c41bc35e022ae9621f28d3
Dane Dalton <danezdalton1@gmail.com>
2022-07-19 17:44:53 -0500

    LinkedList #find(value) finds a value in the LL and returns its index if it exists, or returns nil

--- lib/main.rb
+++ lib/main.rb
@@ -103,6 +103,17 @@ class LinkedList
     return false
   end
 
+  def find(value)
+    i = 0
+    temp = @head
+    until temp == nil
+      return i if temp.value == value
+      i += 1
+      temp = temp.next_node
+    end
+    return nil
+  end
+
   def to_s
     temp = @head
     until temp == nil
@@ -116,6 +127,8 @@ my_list = LinkedList.new()
 my_list.append(8)
 my_list.append(2)
 my_list.append(6)
+my_list.append(43)
+my_list.prepend(9)
 my_list.prepend(77)
 puts "Size: #{my_list.size}"
 puts "Head: #{my_list.head}"
@@ -123,4 +136,5 @@ puts "Tail: #{my_list.tail}"
 puts "At index 2: #{my_list.at(2)}"
 puts "Popped: #{my_list.pop}"
 puts "Contains 2?: #{my_list.contains?(2)}"
+puts "Find index of 6: #{my_list.find(6)}"
 my_list.to_s
