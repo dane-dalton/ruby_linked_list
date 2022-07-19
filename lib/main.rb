cfcec25e2896e8aeae2cfc1c474f0bd9eef9431a
Dane Dalton <danezdalton1@gmail.com>
2022-07-19 16:37:34 -0500

    Linked list and node class established. Append and to_s methods written for LinkedList class

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,42 @@
-class LinkedList
-
-end
-
 class Node
   attr_accessor :value, :next_node
 
   def initialize(value = nil)
     @value = value
-    @next = nil
+    @next_node = nil
   end
 end
+
+class LinkedList
+  attr_accessor :head
+
+  def initialize()
+    @head = nil
+  end
+
+  def append(value)
+    if @head == nil
+      @head = Node.new(value) 
+    else
+      temp = @head
+      until temp.next_node == nil
+        temp = temp.next_node
+      end
+      temp.next_node = Node.new(value)
+    end
+  end
+
+  def to_s
+    temp = @head
+    until temp == nil
+      puts "#{temp.value} => "
+      temp = temp.next_node
+    end
+  end
+end
+
+my_list = LinkedList.new()
+my_list.append(8)
+my_list.append(2)
+my_list.append(6)
+my_list.to_s
